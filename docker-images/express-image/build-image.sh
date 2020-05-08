3ee88c8ebd97dff35934586b083f9aa1cbbf014c
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-05-08 12:22:17 +0200

    Update animals to cities app, build and run update and cities script add

--- docker-images/express-image/build-image.sh
+++ docker-images/express-image/build-image.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t res/express-animals .
+docker build -t res/express-cities .
--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,4 +1,41 @@
+var express = require('express');
 var Chance = require('chance');
+
+var app = express();
 var chance = new Chance();
 
-console.log("Voici un animal : " + chance.animal());
+app.get('/', function(request, response){
+    response.send( generateCities() );
+});
+
+
+app.listen(3000, function(){
+    console.log('Accepting HTTP requests on port 3000');
+});
+
+//console.log("Voici un animal : " + chance.animal());
+
+function generateCities() {
+    var nbOfCities = chance.integer({
+        min: 0,
+        max: 10
+    });
+
+    console.log(nbOfCities);
+
+    var cities = [];
+    for(var i = 0; i < nbOfCities; ++i){
+        var cityName = chance.city();
+        var cityState = chance.state({full:true});
+        var cityZip = chance.zip(); 
+
+        cities.push({
+            name: cityName,
+            state: cityState,
+            zip: cityZip
+        });
+    }
+
+    console.log(cities);
+    return cities;
+}
